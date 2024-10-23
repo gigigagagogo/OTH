2788f6bf5560f9d82aa34354093106a0c8fbbf0f
Samuele Valperta <samuele.valperta@gmail.com>
2024-10-23 23:01:58 +0200

    Bho

--- Compiler_Construction/grammar_exercise/main.c
+++ Compiler_Construction/grammar_exercise/main.c
@@ -6,52 +6,53 @@
 #include "queue.h"
 #include "util.h"
 
-void generate_words(node_g *head, char *startSymbol){
-	node_t *wordQueueHead = NULL, *wordQueueTail = NULL;
-	enqueue_word(&wordQueueHead, &wordQueueTail, startSymbol);
+void generate_words(node_g *head, char *startSymbol) {
+  node_t *wordQueueHead = NULL, *wordQueueTail = NULL;
+  enqueue_word(&wordQueueHead, &wordQueueTail, startSymbol);
 
-	while(!emptyqueue(&wordQueueHead,&wordQueueTail)){
-		char *currentWord=dequeue_word(&wordQueueHead);
-		printf("Word: %s\n", currentWord);
+  while (!emptyqueue(&wordQueueHead)) {
+    printf("ENTRATO\n");
+    char *currentWord = dequeue_word(&wordQueueHead);
+    printf("Word: %s\n", currentWord);
 
-		if(isterminal(currentWord)){
-			node_g *ruleNode=head;
-			
-			while(ruleNode != NULL){
-				int n=0;
-				char *newWord = NULL;
-				do{
-					newWord = rep(currentWord, ruleNode -> rule.lhs, ruleNode -> rule.rhs, n++);
-					if(newWord != NULL){
-						enqueue_word(&wordQueueHead, &wordQueueTail, newWord);
-					
-					}
-					ruleNode = ruleNode -> next;
-				}while(newWord);
-			}
-		}else{
-			printf("Word contains only terminate symbol: %s\n", currentWord);
-		}
-		free(currentWord);
-	}
+    if (!isterminal(currentWord)) {
+      node_g *ruleNode = head;
+
+      while (ruleNode != NULL) {
+
+        int n = 0;
+        char *newWord = NULL;
+        do {
+          newWord =
+              rep(currentWord, ruleNode->rule.lhs, ruleNode->rule.rhs, n++);
+          if (newWord != NULL) {
+            enqueue_word(&wordQueueHead, &wordQueueTail, newWord);
+            printf("%s\n", dequeue_word(&wordQueueHead));
+          }
+        } while (newWord);
+        ruleNode = ruleNode->next;
+      }
+    } else {
+      printf("Word contains only terminate symbol: %s\n", currentWord);
+    }
+    free(currentWord);
+  }
 }
 
+int main(void) {
+  node_g *head = NULL;
+  node_g *tail = NULL;
 
-int main(void){
-	node_g *head = NULL;
-	node_g *tail = NULL;
+  if (generate_rules(&head, &tail) != 0) {
+    printf("Error with rules generation!\n");
+    return 1;
+  }
 
-	if(generate_rules(&head,&tail) != 0){
-		printf("Error with rules generation!\n");
-		return 1;
-	}
-	
-	print_rules(head);
+  print_rules(head);
 
-	char *startSymbol = STARTSYMBOL;
+  char *startSymbol = STARTSYMBOL;
 
-	generate_words(head, startSymbol);
+  generate_words(head, startSymbol);
 
-	return 0; 
-
+  return 0;
 }
--- Compiler_Construction/grammar_exercise/queue.h
+++ Compiler_Construction/grammar_exercise/queue.h
@@ -2,14 +2,13 @@
 #define _QUEUE_H
 
 typedef struct node {
-    struct node *prev;
-    char *word;
-    struct node *next;
+  struct node *prev;
+  char *word;
+  struct node *next;
 } node_t;
 
 int enqueue_word(node_t **head, node_t **tail, char *word);
 char *dequeue_word(node_t **head);
-int emptyqueue(node_t **head, node_t **tail);
-
+int emptyqueue(node_t **head);
 
 #endif /* _QUEUE_H */
